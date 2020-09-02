217a7e6485353aa8e30916a9cd7ea4a67a91148b
Ram_Vaishnav <68468177+RV313@users.noreply.github.com>
2020-09-02 09:28:06 +0530

    Delete testPairToNumber.java

--- colorcoder/testPairToNumber.java
+++
@@ -1,9 +0,0 @@
- static void testPairToNumber(
-        MajorColor major,
-        MinorColor minor,
-        int expectedPairNumber)
-    {
-        int pairNumber = GetPairNumberFromColor(major, minor);
-        System.out.println("Got pair number " + pairNumber);
-        assert(pairNumber == expectedPairNumber);
-    }
